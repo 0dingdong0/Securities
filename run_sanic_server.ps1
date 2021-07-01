6ae749af50193354403f197e1146c59ab2b43c73
odingdongo <odingdongo@hotmail.com>
2021-07-01 17:06:02 +0800

    active debug mode

--- run_sanic_server.ps1
+++ run_sanic_server.ps1
@@ -1,5 +1,5 @@
 cd D:\workspace\python\Securities
 D:\workspace\python\venv_3.9.5\Scripts\activate.ps1
 
-# sanic server.sanic_server.app
+# python -m sanic --debug server.sanic_server.app
 python .\server\sanic_server.py
