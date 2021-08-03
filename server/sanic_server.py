0f5d660d16394813e2054b1ac724d3fdfdad2e46
odingdongo <odingdongo@hotmail.com>
2021-08-03 13:13:11 +0800

    continue impementing sanic_server

--- server/sanic_server.py
+++ server/sanic_server.py
@@ -54,27 +54,34 @@ async def snapshot_handler(results):
         for result in results:
             print(result)
 
+
 # before server start
-
-
 @app.before_server_start
 async def setup_dailydata(app, loop):
     # date = time.strftime('%Y%m%d')
-    date = '20210712'
-    app.ctx.data[date] = DailyData(date)
-    print('before server start')
+    # date = '20210712'
+    # app.ctx.data[date] = DailyData(date)
+    # print('before server start')
+    try:
+        today = time.strftime('%Y%m%d')
+        app.ctx.data[today] = DailyData(today)
+
+        add_snapshot_handler(snapshot_handler)
+        asyncio.create_task(start_snapshot_listening())
+    except FileNotFoundError:
+        print('there is no running dailydata today')
 
 
 @app.after_server_start
 async def add_snapshotting_handler(app, loop):
-    add_snapshot_handler(snapshot_handler)
-    # asyncio.create_task(start_snapshot_listening())
-    asyncio.create_task(start_snapshot_listening(date='20210712'))
-    print('add snapshotting handler')
+    # add_snapshot_handler(snapshot_handler)
+    # # asyncio.create_task(start_snapshot_listening())
+    # asyncio.create_task(start_snapshot_listening(date='20210712'))
+    # print('add snapshotting handler')
+    pass
+
 
 # after server stop
-
-
 @app.after_server_stop
 async def cleanup_dailydata(app, loop):
     print('after_server_stop')
@@ -95,37 +102,39 @@ async def set_user_id(request, response):
         response.cookies['user_id'] = request.ctx.user_id
 
 
+async def get_dailydata(date):
+    if date not in app.ctx.data:
+        file = os.path.join(os.getcwd(), 'storage', f'{date}.hdf5')
+        if os.path.exists(file):
+            app.ctx.data[date] = DailyData.load(dt=date)
+            if not (await app.ctx.ar.get('hq_assist_count')):
+                assist_count = math.ceil(
+                    len(app.ctx.data[date].symbols)/800)+1
+                await app.ctx.ar.set('hq_assist_count', assist_count)
+            assist_count = await app.ctx.ar.get('hq_assist_count')
+
+            msg = {
+                'command': 'compute_statistics',
+                'date': date
+            }
+            await asyncio.gather(
+                *[app.ctx.ar.lpush(f'hq_assist_{_}', json.dumps(msg)) for _ in range(assist_count)]
+            )
+            results = await asyncio.gather(*[app.ctx.ar.brpop(f'hq_assist_{_}_compute_statistics') for _ in range(assist_count)])
+            if not all([json.loads(result[1].decode("utf-8"))['status'] == 'success' for result in results]):
+                raise exceptions.ServerError(
+                    f"Failed to calculate statistics for dailydata at ({date})")
+        else:
+            raise exceptions.NotFound(f"Could not find dailydata at ({date})")
+
+    return app.ctx.data[date]
+
+
 @app.get("/market/<date:\d{8}>")
 async def market(request, date):
-    
-    if date not in app.ctx.data:
-        try:
-            app.ctx.data[date] = DailyData(date)
-        except FileNotFoundError:
-            file = os.path.join(os.getcwd(), 'storage', f'{date}.hdf5')
-            if os.path.exists(file):
-                app.ctx.data[date] = DailyData.load(dt=date)
-                if not (await app.ctx.ar.get('hq_assist_count')):
-                    assist_count = math.ceil(len(app.ctx.data[date].symbols)/800)+1
-                    await app.ctx.ar.set('hq_assist_count', assist_count)
-                assist_count = await app.ctx.ar.get('hq_assist_count')
-                
-                msg = {
-                    'command': 'compute_statistics',
-                    'date': date
-                }
-                await asyncio.gather(
-                    *[app.ctx.ar.lpush(f'hq_assist_{_}', json.dumps(msg)) for _ in range(assist_count)]
-                )
-                results = await asyncio.gather(*[ app.ctx.ar.brpop(f'hq_assist_{_}_compute_statistics') for _ in range(assist_count)])
-                if not all([json.loads(result[1].decode("utf-8"))['status'] =='success' for result in results]):
-                    raise exceptions.ServerError(f"Failed to calculate statistics for dailydata at ({date})")
-            else:
-                raise exceptions.NotFound(f"Could not find dailydata at ({date})")
-            
-    dailydata = app.ctx.data[date]
 
-    
+    dailydata = await get_dailydata(date)
+
     if "timestamp" in request.args:
         timestamp = float(request.args.get("timestamp"))
     else:
@@ -133,9 +142,10 @@ async def market(request, date):
 
     print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(timestamp)))
 
-    result = {'date': date, 'timestamp': timestamp, 'request_id': str(request.id)}
+    result = {'date': date, 'timestamp': timestamp,
+              'request_id': str(request.id)}
     # result = {'date': date, 'timestamp': timestamp}
-    
+
     if timestamp <= dailydata.check_points[0]:
         check_point_idx = 0
     elif timestamp >= dailydata.check_points[-1]:
@@ -143,11 +153,15 @@ async def market(request, date):
     else:
         check_point_idx = np.argmax(dailydata.check_points > timestamp) - 1
 
-    result['zs_indices'] = np.argsort(dailydata.statistic[check_point_idx,:,3]).tolist()
-    result['zt_indices'] = np.argwhere(dailydata.statistic[check_point_idx,:,4]>0).tolist()
-    result['zf_indices'] = np.argsort(dailydata.statistic[check_point_idx,:,0]).tolist()
-    result['lb_indices'] = np.argsort(dailydata.statistic[check_point_idx,:,2]).tolist()
-        
+    result['zs_indices'] = np.argsort(
+        dailydata.statistic[check_point_idx, :, 3]).tolist()
+    result['zt_indices'] = np.argwhere(
+        dailydata.statistic[check_point_idx, :, 4] > 0).tolist()
+    result['zf_indices'] = np.argsort(
+        dailydata.statistic[check_point_idx, :, 0]).tolist()
+    result['lb_indices'] = np.argsort(
+        dailydata.statistic[check_point_idx, :, 2]).tolist()
+
     init = request.args.get("init")
     if init:
         file = 'storage//'+date+'_zhishu.json'
@@ -160,11 +174,12 @@ async def market(request, date):
 
         result['symbols'] = dailydata.symbols.tolist()
         result['names'] = dailydata.names.tolist()
-        result['zt_status'] = list([ 
-            ( 
+        result['zt_status'] = list([
+            (
                 int(idx[0]),
-                int(dailydata.check_points[np.argmax(dailydata.statistic[:check_point_idx+1,idx[0],4]>0)])
-            ) for idx in np.argwhere(dailydata.statistic[check_point_idx,:,4]>0)
+                int(dailydata.check_points[np.argmax(
+                    dailydata.statistic[:check_point_idx+1, idx[0], 4] > 0)])
+            ) for idx in np.argwhere(dailydata.statistic[check_point_idx, :, 4] > 0)
         ])
 
     return response.json(result)
