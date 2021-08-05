2fb826aedf7973dc617203936b03c9f6fd976140
Chang Shu <odingdongo@hotmail.com>
2021-08-05 23:10:20 +0800

    implementing front end

--- server/static/js/index.js
+++ server/static/js/index.js
@@ -29,7 +29,7 @@ window.onload = function(){
 
     let params = {
         init: init,
-        // 'timestamp' : 1626068690
+        // 'timestamp' : 1626059575
     }
 
     axios.get(`/market/${date}`, {'params':params})
@@ -39,5 +39,54 @@ window.onload = function(){
     }).catch((error)=>{
         console.log(error)
     })
+
+    let zhangting = new Zhangting('#zhangting')
 }
 
+
+
+class Zhangting{
+    constructor(parent_selector, left_margin=10, top_margin=10, bottom_margin=10, right_margin=70){
+        this.left_margin = left_margin
+        this.right_margin = right_margin
+        this.top_margin = top_margin
+        this.bottom_margin = bottom_margin
+
+        this.div_parent = document.querySelector(parent_selector)
+        this.svg = d3.select(parent_selector).select('svg')
+        this.svg.attr('width', this.div_parent.clientWidth).attr('height', this.div_parent.clientHeight)
+
+        this.draw_axis()
+    }
+
+    draw_axis(){
+        let width = this.div_parent.clientWidth
+        let height = this.div_parent.clientHeight
+
+        let count = 8
+        let step = (width-this.left_margin-this.right_margin)/count
+        let lines = []
+
+        for(let i=0; i<=count; i++){
+            let x = this.left_margin+i*step
+            let y1 = this.top_margin
+            let y2 = height-this.bottom_margin
+
+            lines.push([x, y1, x, y2])
+        }
+
+        this.svg.append('g')
+                .attr('class', 'time-lines')
+                .selectAll("line")
+                .data(lines)
+                .join('line')
+                .attr('x1', d=>d[0])
+                .attr('y1', d=>d[1])
+                .attr('x2', d=>d[2])
+                .attr('y2', d=>d[3])
+                .style('stroke', (d,i)=> i==4 ? 'lightgrey' : 'gray')
+                .style('stroke-width', 0.5)
+    }
+
+
+}
